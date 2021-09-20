f7431c774a7ecb83c61539f1e7f333cae401ce7c
ceejayv328 <ceejayv328@gmail.com>
2021-09-20 17:59:18 +0800

    Added controller for National Park

---
+++ ParkyAPI/Controllers/NationalParkController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Repositories.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NationalParkController : Controller
+    {
+        private INationalParkRepository _nationalParkRepository;
+        private readonly IMapper _mapper;
+
+        public NationalParkController(INationalParkRepository nationalParkRepository, IMapper mapper)
+        {
+            _nationalParkRepository = nationalParkRepository;
+            _mapper = mapper;
+        }
+    }
+}
--- ParkyAPI/Repositories/IRepositories/INationalParkRepository.cs
+++ ParkyAPI/Repositories/IRepositories/INationalParkRepository.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ParkyAPI.Repositories.IRepository
+namespace ParkyAPI.Repositories.IRepositories
 {
     public interface INationalParkRepository
     {
