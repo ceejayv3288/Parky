c9e1f6a6be9cfaeb2576edbe18d35ffd2dbfb463
ceejayv328 <ceejayv328@gmail.com>
2021-09-23 18:20:52 +0800

    Get All national parks API call

---
+++ ParkyAPI/Controllers/NationalParksController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Models.Dtos;
+using ParkyAPI.Repositories.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NationalParksController : Controller
+    {
+        private INationalParkRepository _nationalParkRepository;
+        private readonly IMapper _mapper;
+
+        public NationalParksController(INationalParkRepository nationalParkRepository, IMapper mapper)
+        {
+            _nationalParkRepository = nationalParkRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetNationalParks()
+        {
+            var objList = _nationalParkRepository.GetNationalParks();
+
+            var objDto = new List<NationalParkDto>();
+            foreach (var obj in objList)
+            {
+                objDto.Add(_mapper.Map<NationalParkDto>(obj));
+            }
+
+            return Ok(objDto);
+        }
+    }
+}
