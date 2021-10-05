bf2e837d295dad18a407ff263cd2b24bf36bbd9a
ceejayv328 <ceejayv328@gmail.com>
2021-10-05 18:19:29 +0800

    Added Trail Repository and Interface

---
+++ ParkyAPI/Repositories/TrailRepository.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkyAPI.Data;
+using ParkyAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Repositories
+{
+    public class TrailRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TrailRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool CreateTrail(Trail trail)
+        {
+            _db.Trails.Add(trail);
+            return Save();
+        }
+
+        public bool DeleteTrail(Trail trail)
+        {
+            _db.Trails.Remove(trail);
+            return Save();
+        }
+
+        public Trail GetTrail(int trailId)
+        {
+            return _db.Trails.Include(c => c.NationalPark).FirstOrDefault(x => x.Id == trailId);
+        }
+
+        public ICollection<Trail> GetTrails()
+        {
+            return _db.Trails.Include(c => c.NationalPark).OrderBy(x => x.Name).ToList();
+        }
+
+        public bool TrailExists(string name)
+        {
+            bool value = _db.Trails.Any(x => x.Name.ToLower().Trim() == name.ToLower().Trim());
+            return value;
+        }
+
+        public bool TrailExists(int id)
+        {
+            return _db.Trails.Any(x => x.Id == id);
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() >= 0 ? true : false;
+        }
+
+        public bool UpdateTrail(Trail trail)
+        {
+            _db.Trails.Update(trail);
+            return Save();
+        }
+
+        public ICollection<Trail> GetTrailsInNationalPark(int nationalParkId)
+        {
+            return _db.Trails.Include(c => c.NationalPark).Where(c => c.NationalParkId == nationalParkId).ToList();
+        }
+    }
+}
