7d1b1a5beae68fc52af35a6cad03dc3b864e53c6
ceejayv328 <ceejayv328@gmail.com>
2021-10-08 17:32:10 +0800

    Created separate Dto for Upserting  trail

--- ParkyAPI/Mappers/ParkyMapper/ParkyMappings.cs
+++ ParkyAPI/Mappers/ParkyMapper/ParkyMappings.cs
@@ -13,6 +13,8 @@ namespace ParkyAPI.Mappers.ParkyMapper
         public ParkyMappings()
         {
             CreateMap<NationalPark, NationalParkDto>().ReverseMap();
+            CreateMap<Trail, TrailDto>().ReverseMap();
+            CreateMap<Trail, TrailUpsertDto>().ReverseMap();
         }
     }
 }
