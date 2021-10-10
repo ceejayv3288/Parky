36428162f691f9e9fafca72fa68cb19328efe207
ceejayv328 <ceejayv328@gmail.com>
2021-10-10 18:53:24 +0800

    Added javascript to load table and edited NationalParksController view to display table

---
+++ ParkyWeb/wwwroot/js/nationalPark.js
@@ -0,0 +1,33 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+})
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/nationalParks/GetAllNationalParks",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "50%" },
+            { "data": "state", "width": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                                <a href"/nationalParks/Upsert/${data}" class='btn btn-success text-white'
+                                    style='cursor:pointer;'> <i class='far fa-edit'></i></a>
+                                    &nbsp;
+                                <a onclick=Delete("/nationalParks/Delete/${data}") class='btn btn-danger text-white'
+                                    style='cursor:pointer;'> <i class='far fa-trash-alt'></i></a>
+                                    &nbsp;
+                            </div>`;
+                },
+                "width": "30%"
+            }
+        ]
+    });
+}
