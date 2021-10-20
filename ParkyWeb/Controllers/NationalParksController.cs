a0dc93d729bcd7a9b87833f164d8b1e9bdbee2ee
ceejayv328 <ceejayv328@gmail.com>
2021-10-20 15:57:20 +0800

    Fix input bug and added inserting of image

--- ParkyWeb/Controllers/NationalParksController.cs
+++ ParkyWeb/Controllers/NationalParksController.cs
@@ -3,6 +3,7 @@ using ParkyWeb.Models;
 using ParkyWeb.Repositories.IRepositories;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -43,6 +44,47 @@ namespace ParkyWeb.Controllers
         public async Task<IActionResult> GetAllNationalParks()
         {
             return Json(new { data = await _nationalParkRepository.GetAllAsync(StaticDetails.NationalParkAPIPath) });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(NationalPark obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+                    obj.Picture = p1;
+                }
+                else
+                {
+                    var objFromDb = await _nationalParkRepository.GetAsync(StaticDetails.NationalParkAPIPath, obj.Id);
+                    obj.Picture = objFromDb.Picture;
+                }
+                if (obj.Id == 0)
+                {
+                    await _nationalParkRepository.CreateAsync(StaticDetails.NationalParkAPIPath, obj);
+                }
+                else
+                {
+                    await _nationalParkRepository.UpdateAsync(StaticDetails.NationalParkAPIPath + obj.Id, obj);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(obj);
+            }
         }
     }
 }
