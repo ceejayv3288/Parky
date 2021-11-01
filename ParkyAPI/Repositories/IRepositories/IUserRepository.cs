6490c7abf0ee7d71dd62fae928ec7f09eca26426
ceejayv328 <ceejayv328@gmail.com>
2021-11-01 17:44:58 +0800

    Added User repository

---
+++ ParkyAPI/Repositories/IRepositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using ParkyAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Repositories.IRepositories
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+        User Authenticate(string username, string passwork);
+        User Register(string username, string password);
+    }
+}
---
+++ ParkyAPI/Repositories/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using ParkyAPI.Data;
+using ParkyAPI.Models;
+using ParkyAPI.Repositories.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public User Authenticate(string username, string passwork)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Register(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
